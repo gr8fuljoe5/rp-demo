332fbb31ff7dc7526dc5d96e139a1429fb88a8c9
Joe Kanakaraj <jkanakaraj@gmail.com>
2021-05-12 08:00:31 -0400

    Content

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,46 +1,66 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
 import Head from "next/head";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Header(props) {
+  const classes = useStyles();
   return (
-    <div>
+    <section>
       <Head>
         <title>{props.title || "Next.js Demo"}</title>
       </Head>
-      <header className={styles.container}>
-        <Link href={"/"}>
-          <a>
-            <h1>RP Next.JS demo </h1>
-          </a>
-        </Link>
-
-        <nav className={styles.nav}>
-          <ul>
-            <li>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              <Link href={"/"}>
+                <a>NextJS Demo</a>
+              </Link>
+            </Typography>
+            <MenuItem>
               <Link href={"/basics"}>
                 <a>Basics</a>
               </Link>
-            </li>
-            <li>
+            </MenuItem>
+            <MenuItem>
               <Link href={"/data-fetching/blog"}>
                 <a>Data Fetching Blog Demo</a>
               </Link>
-            </li>
-            <li>
+            </MenuItem>
+            <MenuItem>
               <Link href={"/icr"}>
                 <a>ICR</a>
               </Link>
-            </li>
-            <li>
+            </MenuItem>
+            <MenuItem>
               <Link href={"/dynamic-imports"}>
                 <a> Dynamic Imports</a>
               </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+            </MenuItem>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </section>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Header from "../components/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,10 +9,44 @@ export default function Home() {
         <title>RP Next.js Demo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <div className={styles.grid}>
+          <Link href={"/basics"}>
+            <a className={styles.card}>
+              <h3>The Basics &rarr;</h3>
+              <p>Routing, Built-in CSS Support, Image Optimization </p>
+            </a>
+          </Link>
+
+          <Link href={"/data-fetching"}>
+            <a className={styles.card}>
+              <h3>Data-Fetching &rarr;</h3>
+              <p>See some SSG, SSR and ICR examples in action</p>
+            </a>
+          </Link>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Dynamic Imports &rarr;</h3>
+            <p>Learn about the power of dynamic imports</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+
+        <h1 className={styles.title}>
+          <a href="https://nextjs.org">Next.js Docs</a>
         </h1>
 
         <div className={styles.grid}>
