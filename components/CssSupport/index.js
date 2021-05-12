60ec82fc016776ef074bea24c626eba654410a8f
Joe Kanakaraj <jkanakaraj@gmail.com>
2021-05-12 08:19:41 -0400

    Cleanup imports

--- components/CssSupport/index.js
+++ components/CssSupport/index.js
@@ -5,31 +5,37 @@ import { Card, Grid } from "@material-ui/core";
 
 function Index(props) {
   return (
-    // <Grid className={styles.main} container spacing={2}>
-    //   <Grid item xs={6}>
-    //     <Card className={styles.container}>Css Modules Support</Card>
-    //   </Grid>
-    //
-    //   <Grid item xs={6}>
-    //     <Card className={sass.container}>Css Sass Support</Card>
-    //   </Grid>
-    //   <Grid item xs={12}>
-    //     <p>Next also supports Less, Stylus, Tailwind and CSS-in-JS options. </p>
-    //   </Grid>
-    // </Grid>
+    <>
+      {/*<Grid className={styles.main} container spacing={2}>*/}
+      {/*  <Grid item xs={6}>*/}
+      {/*    <Card className={styles.container}>Css Modules Support</Card>*/}
+      {/*  </Grid>*/}
 
-    <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <Card>Css Modules Support</Card>
-      </Grid>
+      {/*  <Grid item xs={6}>*/}
+      {/*    <Card className={sass.container}>Css Sass Support</Card>*/}
+      {/*  </Grid>*/}
+      {/*  <Grid item xs={12}>*/}
+      {/*    <p>*/}
+      {/*      Next also supports Less, Stylus, Tailwind and CSS-in-JS options.{" "}*/}
+      {/*    </p>*/}
+      {/*  </Grid>*/}
+      {/*</Grid>*/}
 
-      <Grid item xs={6}>
-        <Card>Css Sass Support</Card>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Card>Css Modules Support</Card>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Card>Css Sass Support</Card>
+        </Grid>
+        <Grid item xs={12}>
+          <p>
+            Next also supports Less, Stylus, Tailwind and CSS-in-JS options.{" "}
+          </p>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <p>Next also supports Less, Stylus, Tailwind and CSS-in-JS options. </p>
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
