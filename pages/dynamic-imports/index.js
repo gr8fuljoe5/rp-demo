8fe2c9bb2c4d20c2a0d8f6305d1b9f0e2be6a076
Joe Kanakaraj <jkanakaraj@gmail.com>
2021-05-12 11:30:07 -0400

    Cleanup dynamic imports presentation

--- pages/dynamic-imports/index.js
+++ pages/dynamic-imports/index.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { Container } from "@material-ui/core";
+import { Container, Card, Grid, Typography } from "@material-ui/core";
 import Header from "../../components/Header";
-import Fuse from "../../components/Dynamic/fuse";
 import dynamic from "next/dynamic";
 
 // import Plot from "react-plotly.js";
@@ -13,6 +12,10 @@ const DynamicComponent1 = dynamic(() =>
 const DynamicComponent2 = dynamic(() =>
   import("../../components/Dynamic/hello2")
 );
+
+const FuseComponent = dynamic(() => {
+  return import("../../components/Dynamic/fuse");
+});
 
 // const Plot = dynamic(
 //   () => {
@@ -28,35 +31,62 @@ const IndexPage = () => {
   return (
     <Container maxWidth>
       <Header title={"Dynamic Imports"} />
-
-      {/* Load immediately, but in a separate bundle */}
-      <DynamicComponent1 />
-
-      {/* This component will never be loaded */}
-      {falsyField && <DynamicComponent1 />}
-
-      {/* Load on demand */}
-      {showMore && <DynamicComponent2 />}
-      <button onClick={() => setShowMore(!showMore)}>Show Component</button>
-
-      {/*<section>*/}
-      {/*  <Plot*/}
-      {/*    data={[*/}
-      {/*      {*/}
-      {/*        x: [1, 2, 3],*/}
-      {/*        y: [2, 6, 3],*/}
-      {/*        type: "scatter",*/}
-      {/*        mode: "lines+markers",*/}
-      {/*        marker: { color: "red" },*/}
-      {/*      },*/}
-      {/*      { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },*/}
-      {/*    ]}*/}
-      {/*    layout={{ width: 320, height: 240, title: "A Fancy Plot" }}*/}
-      {/*  />*/}
-      {/*</section>*/}
-
-      {/* Load library on demand */}
-      <Fuse />
+      <Grid container spacing={2}>
+        <Grid>
+          <Typography variant={"h2"}>Dynamic Imports</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant={"body1"}>
+            Load immediately, but in a separate bundle
+          </Typography>
+          {/* Load immediately, but in a separate bundle */}
+          <Card>
+            <DynamicComponent1 />
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant={"body1"}>
+            This component will never be loaded
+          </Typography>
+          {/* This component will never be loaded */}
+          <Card>{falsyField && <DynamicComponent1 />}</Card>
+        </Grid>
+        <Grid item xs={12}>
+          {/* Load on demand */}
+          <Card>
+            <Typography variant={"body1"}>Loaded on demand</Typography>
+            {showMore && <DynamicComponent2 />}
+            <button onClick={() => setShowMore(!showMore)}>
+              Show Component
+            </button>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          {/* Load library on demand */}
+          <Typography variant={"body1"}>
+            Load 3rd party library on demand{" "}
+          </Typography>
+          <Card>
+            <FuseComponent />
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant={"body1"}>Loads with no SSR</Typography>
+          {/*  <Plot*/}
+          {/*    data={[*/}
+          {/*      {*/}
+          {/*        x: [1, 2, 3],*/}
+          {/*        y: [2, 6, 3],*/}
+          {/*        type: "scatter",*/}
+          {/*        mode: "lines+markers",*/}
+          {/*        marker: { color: "red" },*/}
+          {/*      },*/}
+          {/*      { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },*/}
+          {/*    ]}*/}
+          {/*    layout={{ width: 320, height: 240, title: "A Fancy Plot" }}*/}
+          {/*  />*/}
+        </Grid>
+      </Grid>
     </Container>
   );
 };
